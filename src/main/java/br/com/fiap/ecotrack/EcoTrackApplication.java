ccf68d9d13ec61f31f0a696d3c99d27c8374fd3b
Mateus Bhering <mateusbbhering@gmail.com>
2026-09-19 12:43:59 -0300

    Atualiza actions para versoes atuais, Maven 3.9.16 e titulo da OpenAPI
    - actions/checkout v7, setup-java v6, upload-artifact v7, docker/* v4/v7 (remove avisos de Node 20)
    - Dependabot ignora saltos de versao principal (Spring Boot 4, Java 24)
    - Pipeline ignora commits somente de documentacao (paths-ignore)

--- src/main/java/br/com/fiap/ecotrack/EcoTrackApplication.java
+++ src/main/java/br/com/fiap/ecotrack/EcoTrackApplication.java
@@ -1,9 +1,15 @@
 package br.com.fiap.ecotrack;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "EcoTrack API - Cidades ESGInteligentes",
+        version = "1.0.0",
+        description = "Inventario de emissoes de gases de efeito estufa (GHG Protocol) de cidades e organizacoes"))
 public class EcoTrackApplication {
 
     public static void main(String[] args) {
